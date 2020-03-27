8f7ff34af18982855ba7d3162a8f47e6da11c2f7
Austin Shafer <ashafer@badland.io>
2020-03-26 21:36:39 -0400

    Fix username querying

--- playground/http_server.py
+++ playground/http_server.py
@@ -6,26 +6,37 @@ from aiohttp import web
 from kademlia.network import Server
 
 kad_port = -1
+our_username = ""
 routes = web.RouteTableDef()
 
-@routes.get('/')
-async def hello(request):
+def get_our_profile():
     profile = {}
     profile['username'] = 'cwheezer'
     profile['fullname'] = 'Carl Wheezer'
+    return profile
+
+@routes.get('/')
+async def hello(request):
+    profile = get_our_profile()
     return web.Response(text=json.dumps(profile))
 
 @routes.get('/user/{username}')
 async def get_user_profile(request):
+    username = request.match_info['username']
     kad = Server()
     await kad.listen(8889)
     print('querying network at {}:{}', 'localhost', kad_port)
     await kad.bootstrap([('localhost', kad_port)])
 
-    profile = await kad_client.get_user_profile(kad, request.match_info['username'])
-    print(profile.json())
+    profile = {}
+    if username == our_username:
+        profile = get_our_profile()
+    else:
+        profile = await kad_client.get_user_profile(kad, username)
+        profile = profile.json()
+    print(profile)
     kad.stop()
-    return web.Response(text=str(profile.json()))
+    return web.Response(text=str(profile))
 
 # apparently we need to use the asyncio application
 # runner to set the address... weird
@@ -40,6 +51,8 @@ async def run_site(args, app):
 # entrypoint from sad.py
 def http_server_worker_thread(args):
     global kad_port
+    global our_username
+    our_username = args['username']
     kad_port = args['network_port']
     aio = asyncio.get_event_loop()
 
