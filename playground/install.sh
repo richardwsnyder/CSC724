39056f14d1271111291b21e5574f6ad0bb6a6200
Austin Shafer <ashafer@badland.io>
2020-04-18 14:28:51 -0400

    add firewall rules open port 80

--- playground/install.sh
+++ playground/install.sh
@@ -3,3 +3,5 @@
 git clone https://github.com/richardwsnyder/CSC724
 apt install python3-pip
 python3 -m pip install kademlia toml django
+iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
